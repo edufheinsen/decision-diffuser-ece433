53808c66bf3ebe4eaf91ec9733ef08ff5362a1d8
Eduardo A. Fernandez <eduardof@princeton.edu>
2024-04-14 18:20:26 -0400

    Change hard-coded weights directory. Add comment

--- code/config/locomotion_config.py
+++ code/config/locomotion_config.py
@@ -6,7 +6,7 @@ class Config(ParamsProto):
     # misc
     seed = 100
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    bucket = '/home/dbq/projects/weights/'
+    bucket = '/home/eduardof/weights' # Create a '/home/$YOUR_NETID/weights' directory
     dataset = 'hopper-medium-expert-v2'
 
     ## model
